9a09d15a4ba263c9d8016586aa49854e3b812401
saragarci <s@saragarci.com>
2023-12-06 13:13:44 +0100

    [IMP] minor refactor in adjacencyMatrix

--- src/utils/adjacencyMatrix.ts
+++ src/utils/adjacencyMatrix.ts
@@ -18,33 +18,35 @@ class AdjacencyMatrix {
         const adjacencyMatrix = []
         for (let i = 0; i < n; i++) {
             const row = new Array(n).fill(0)
-            adjacencyMatrix.push(row)
+            adjacencyMatrix.push(row) 
         }
 
         for (let i = 0; i < n; i++) {
             for (let j = i + 1; j < n; j++) {
                 if (type === NetworkType.Small) {
                     // For small, it's fully connected
-                    adjacencyMatrix[i][j] = adjacencyMatrix[j][i] = Math.ceil(Math.random() * 10)
+                    const weight = Math.ceil(Math.random() * 10)
+                    adjacencyMatrix[i][j] = adjacencyMatrix[j][i] = weight
                 } else {
                     // For medium and large, limit connections to 2-4
                     const probability = type === NetworkType.Medium ? 0.01 : 0.001
-                    if (Math.random() < probability) { // You can change this probability
-                        adjacencyMatrix[i][j] = adjacencyMatrix[j][i] = Math.ceil(Math.random() * 10)
+                    if (Math.random() < probability) {
+                        const weight = Math.ceil(Math.random() * 10)
+                        adjacencyMatrix[i][j] = adjacencyMatrix[j][i] = weight
                     }
                 }
             }
         }
 
         // Ensure 2-4 connections for medium and large sizes
-        // TO-DO: fixme
         if (type === NetworkType.Medium || type === NetworkType.Large) {
             for (let i = 0; i < n; i++) {
                 let connectedNodes = adjacencyMatrix[i].filter(val => val > 0).length
                 while (connectedNodes < 2) {
                     let j = Math.floor(Math.random() * n)
                     if (j !== i && adjacencyMatrix[i][j] === 0) {
-                        adjacencyMatrix[i][j] = adjacencyMatrix[j][i] = Math.ceil(Math.random() * 10)
+                        const weight = Math.ceil(Math.random() * 10)
+                        adjacencyMatrix[i][j] = adjacencyMatrix[j][i] = weight
                         connectedNodes++
                     }
                 }
